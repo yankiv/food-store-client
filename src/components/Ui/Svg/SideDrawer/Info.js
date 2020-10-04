4c56a4e85213ee921376c695472cc70889be08bb
Nick Yankiv <yankivnick@gmail.com>
2020-10-04 18:30:06 +0300

    Fix svg

--- src/components/Ui/Svg/SideDrawer/Info.js
+++ src/components/Ui/Svg/SideDrawer/Info.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const info = () => (
     <svg width="18" height="20" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M9 8.5V14" stroke="#616161" stroke-width="1.0006" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+        <path d="M9 8.5V14" stroke="#616161" strokeWidth="1.0006" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"></path>
         <path d="M9 7C9.27614 7 9.5 6.77614 9.5 6.5C9.5 6.22386 9.27614 6 9 6C8.72386 6 8.5 6.22386 8.5 6.5C8.5 6.77614 8.72386 7 9 7Z" fill="#616161"></path>
         <circle cx="9" cy="10" r="8.5" stroke="#616161"></circle>
     </svg>
